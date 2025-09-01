8b26e41dcd8b328b31da9194808041f7829a7014
Ali Gohar <142131055+AliGohar2151@users.noreply.github.com>
2025-09-01 18:22:36 +0500

    Refactors output directory for normalized events
    Updates the output directory to store the normalized event data.
    This change improves the organization of processed data by separating
    normalized events into a dedicated directory.

--- ga4-data/script/events_normalization.py
+++ ga4-data/script/events_normalization.py
@@ -3,14 +3,14 @@ import pandas as pd
 import gzip
 import pathlib
 
-# Base directory = location of this script
 base_dir = pathlib.Path(__file__).parent
 
-# Input & output folders
 input_dir = (
     base_dir / "../ga4-raw-data/events-json/analytics_291746817/2024/10"
 ).resolve()
-output_dir = (base_dir / "../ga4-clean/analytics291746817/2024/10").resolve()
+output_dir = (
+    base_dir / "../ga4-clean/events-normalized/analytics291746817/2024/10"
+).resolve()
 output_dir.mkdir(parents=True, exist_ok=True)
 
 print("Looking in:", input_dir)
@@ -21,10 +21,8 @@ def process_file(file_path):
     with gzip.open(file_path, "rt", encoding="utf-8") as f:
         data = json.load(f)
 
-    # Flatten JSON
     data_df = pd.json_normalize(data, sep="_")
 
-    # Extract event_params → prefix with "param_"
     params_data = [
         {item["key"]: next(v for v in item["value"].values() if v is not None)}
         for params_list in data_df["event_params"]
@@ -32,7 +30,6 @@ def process_file(file_path):
     ]
     params_df = pd.DataFrame(params_data).add_prefix("ep_")
 
-    # Extract user_properties → prefix with "user_prop_"
     user_props_data = [
         {
             prop["key"]: next(
@@ -46,12 +43,10 @@ def process_file(file_path):
     ]
     user_props_df = pd.DataFrame(user_props_data).add_prefix("user_prop_")
 
-    # Add year & month
     event_date_dt = pd.to_datetime(data_df["event_date"], format="%Y%m%d")
     data_df["year"] = event_date_dt.dt.year
     data_df["month"] = event_date_dt.dt.month
 
-    # Final DataFrame (safe from duplicate column names)
     final_df = pd.concat([data_df, params_df, user_props_df], axis=1)
     final_df.drop(columns=["user_properties", "event_params"], inplace=True)
 
@@ -66,15 +61,12 @@ def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-# Process all .json files in the folder
 for file_path in input_dir.glob("*.json"):
     print(f"Processing {file_path.name} ...")
     df = process_file(file_path)
 
-    # Clean dtypes for Parquet
     df = clean_dataframe(df)
 
-    # Save with same name but .parquet extension
     output_file = output_dir / f"{file_path.stem}.parquet"
     df.to_parquet(output_file, index=False)
     print(f"Saved {output_file}")
