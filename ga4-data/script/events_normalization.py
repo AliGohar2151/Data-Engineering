30e6d6814fdab68253b03ec2494f7c5779b83fe3
Ali Gohar <142131055+AliGohar2151@users.noreply.github.com>
2025-09-02 12:08:10 +0500

    Refactors event parameter extraction
    Improves the extraction of event parameters from the JSON data.
    
    The previous implementation had a nested loop structure that was inefficient. This change flattens the loop, making the extraction more concise and efficient.
    
    Also corrects a typo in a variable name to improve readability.

--- ga4-data/script/events_normalization.py
+++ ga4-data/script/events_normalization.py
@@ -27,11 +27,13 @@ def process_file(file_path):
     data_df = pd.json_normalize(data, sep="_")
 
     params_data = [
-        {item["key"]: next(v for v in item["value"].values() if v is not None)}
+        {
+            item["key"]: next(v for v in item["value"].values() if v is not None)
+            for item in params_list
+        }
         for params_list in data_df["event_params"]
-        for item in params_list
     ]
-    params_df = pd.DataFrame(params_data).add_prefix("ep_")
+    param_df = pd.DataFrame(params_data).add_prefix("ep_")
 
     user_props_data = [
         {
@@ -50,7 +52,7 @@ def process_file(file_path):
     data_df["year"] = event_date_dt.dt.year
     data_df["month"] = event_date_dt.dt.month
 
-    final_df = pd.concat([data_df, params_df, user_props_df], axis=1)
+    final_df = pd.concat([data_df, param_df, user_props_df], axis=1)
     final_df.drop(columns=["user_properties", "event_params"], inplace=True)
 
     return final_df
@@ -62,6 +64,13 @@ def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
         if df[col].dtype == "object":
             df[col] = df[col].astype(str)
     return df
+
+    # df = process_file(
+    #     "D:\\Data-Engineering\\ga4-data\\ga4-raw-data\\events-json\\analytics_291746817\\2024\\10\\events_20241001.json"
+    # )
+    # df = clean_dataframe(df)
+    # df.to_parquet("output.parquet", index=False)
+    print(f"Saved {output_file}")
 
 
 for file_path in input_dir.glob("*.json"):
