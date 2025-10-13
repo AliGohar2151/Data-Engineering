13792255a841ec228f3dbf08b37fb29521313ff1
Ali Gohar <142131055+AliGohar2151@users.noreply.github.com>
2025-10-13 09:40:28 +0500

    Added code to run processing scripts Added config code for mariadb client

--- ga4-data-processing/src/config/settings.py
+++ ga4-data-processing/src/config/settings.py
@@ -1,12 +1,3 @@
-# DB_CONFIG = {
-#     "host": "localhost",
-#     "user": "root",
-#     "password": "1234",
-#     "database": "ga4_db",
-#     "port": 3306,
-# }
-
-
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
