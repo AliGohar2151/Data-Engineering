5a221bdb4a7b87a6248a8f686d3449612df48b8f
Ali Gohar <142131055+AliGohar2151@users.noreply.github.com>
2025-10-14 13:59:15 +0500

    modified:   ga4-data-processing/src/rules/mapping_rules.py

--- ga4-data-processing/src/rules/mapping_rules.py
+++ ga4-data-processing/src/rules/mapping_rules.py
@@ -21,8 +21,7 @@ class MappingRules:
             mt.title,
             tf.field_priority,
             ep.event_name,
-            ep.ep,
-            tf.data_type
+            
         FROM
             ga4_org_model_table_fields tf
         LEFT JOIN ga4_org_model_tables mt 
